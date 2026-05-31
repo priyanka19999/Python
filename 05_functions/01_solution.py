7a24c083f5830a1bbaa27c89f5238067c7979dc1
Priyanka Priyadarshani <137867082+priyanka19999@users.noreply.github.com>
2026-05-31 22:49:36 +0530

    Basic Function Syntax
    Basic Function Syntax

---
+++ 05_functions/01_solution.py
@@ -0,0 +1,14 @@
+#Problem: Write a function to calculate and return the square of a number.
+def square_of_num(number):
+   print(number ** 2)
+square_of_num(9)
+
+#Ans: 81
+
+
+#2nd way (storing the function in a variable i.e result)
+def square_of_num(number):
+   return number ** 2
+result = square_of_num(6)
+print(result)
+#Ans: 36
